79d2fa1b0eb10d3885da8857761924e22782ccce
brenetic <b@brenetic.com>
2018-03-19 18:12:09 +0000

    So it begins. Again.
    Swap to use a python based bot framework.

---
+++ config.py
@@ -0,0 +1,28 @@
+import logging
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+BACKEND = os.getenv('BACKEND', 'Text')
+
+BOT_DATA_DIR = join(dirname(__file__), 'data')
+BOT_EXTRA_PLUGIN_DIR = join(dirname(__file__), 'plugins')
+
+BOT_LOG_FILE = join(dirname(__file__), 'errbot.log')
+BOT_LOG_LEVEL = logging.DEBUG
+
+BOT_ADMINS = (os.getenv('ADMINS', default=None))
+
+BOT_ASYNC = True
+BOT_ASYNC_POOLSIZE = 10
+
+BOT_IDENTITY = {
+  'token': os.getenv('TOKEN', default=None)
+}
+
+BOT_PREFIX_OPTIONAL_ON_CHAT = True
+BOT_ALT_PREFIXES = ('@dorothy',)
+CHATROOM_PRESENCE = ()
